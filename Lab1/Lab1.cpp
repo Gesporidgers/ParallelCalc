e46afbbd24e27a15d89454f4bc10528961ea7c73
Gesporidgers <49988107+Gesporidgers@users.noreply.github.com>
2026-03-05 18:56:49 +0300

    Доделан атомик

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 #include <fstream>
 #include <omp.h>
+#include <iomanip>
 #include <string>
 #include <vector>
 
@@ -34,15 +35,20 @@ double parallel_sum(double* arr, int n) {
 }
 
 double critical_sum(double* arr, int n) {
-
 	double sum = 0;
-#pragma omp parallel for
-	for (int i = 0; i < n; i++) {
+#pragma omp parallel
+	{
+		double local_sum = 0;
+#pragma omp for
+		for (int i = 0; i < n; i++) {
+			local_sum += arr[i];
+		}
 #pragma omp critical
 		{
-		sum += arr[i];
+			sum += local_sum;
 		}
 	}
+	
 	return sum;
 }
 
@@ -67,14 +73,15 @@ int main()
 	{
 		int id = omp_get_thread_num();
 	}
-	outFile << "N,Ts,Tred,Tcrit,Sred,Ered,Scrit,Ecrit\n";
-	vector<int> Ns{ 10000,1000000,10000000/*,1000000000*/ }; //размерности массивов
+	outFile << "N,Ts,Tred,Tcrit,Tatom,Sred,Ered,Scrit,Ecrit,Satom,Eatom\n";
+	outFile << fixed << setprecision(6);
+	vector<int> Ns{ 10000,100000,1000000,10000000/*,1000000000*/ }; //размерности массивов
 	for (int N : Ns) {
 		cout << "Calculating for N=" << N << endl; // для понимания что делает программа
 		double* arr = new double[N];
 		generate_array(arr, N); // создаём массив
 		double sum;
-		double timeseq=0, timepar=0, timecrit=0, sred=0, ered=0, scrit=0, ecrit=0;
+		double timeseq=0, timepar=0, timecrit=0, sred=0, ered=0, scrit=0, ecrit=0, timeatom=0, satom=0, eatom=0;
 
 		
 		for (int i = 0; i < 20; i++) {
@@ -95,12 +102,21 @@ int main()
 			sum = critical_sum(arr, N);
 			end = omp_get_wtime();
 			double tc = end - start;
+
+			//прогон параллельный но с атомарной операцией
+			start = omp_get_wtime();
+			sum = critical_sum(arr, N);
+			end = omp_get_wtime();
+			double ta = end - start;
 			cout << '.'; // подобие полосы загрузки для понимания
 			// прибавляем получившееся для последующего усреднения
-			timeseq += ts; timepar += tp; timecrit += tc; sred += timeseq / timepar; ered += timeseq / (timepar * 16); scrit += timeseq / timecrit; ecrit += timeseq / (timecrit * 16);
+			timeseq += ts; timepar += tp; timecrit += tc; timeatom += ta; sred += timeseq / timepar; ered += timeseq / (timepar * 16); scrit += timeseq / timecrit; ecrit += timeseq / (timecrit * 16); satom += timeseq / timeatom; eatom += timeseq / (timeatom * 16);
 		}
 		delete[] arr;
-		outFile << N << ',' << timeseq / 20 << ',' << timepar / 20 << ',' << timecrit / 20 << ',' << sred / 20 << ',' << ered / 20 << ',' << scrit / 20 << ',' << ecrit / 20 << '\n';
+		outFile << N << ',' << timeseq / 20 << ',' << timepar / 20 << ',' << timecrit / 20 << ','<< timeatom / 20 << ',' 
+			<< sred / 20 << ',' << ered / 20 << ',' 
+			<< scrit / 20 << ',' << ecrit / 20<< ',' 
+			<< satom / 20 << ',' << eatom / 20 << '\n';
 		cout << endl; // вывод в csv файл усреднённых значений
 	}
 	outFile.close();
